4b1f652d40f348b1a4600d9b489918426da97dc8
scoty755 <scoty755@gmail.com>
2017-01-23 19:35:42 +0900

    b3uhl: Update some files

---
+++ mkbootimg.mk
@@ -0,0 +1,32 @@
+LOCAL_PATH := $(call my-dir)
+
+ifdef TARGET_PREBUILT_DTB
+	BOARD_MKBOOTIMG_ARGS += --dt $(TARGET_PREBUILT_DTB)
+endif
+
+PREBUILT_MKBOOTIMG := $(LOCAL_PATH)/mkbootimg
+
+INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
+$(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_RAMDISK_TARGET)
+	@echo ----- Making recovery image ------
+	$(hide) $(PREBUILT_MKBOOTIMG) \
+		--kernel $(TARGET_PREBUILT_KERNEL) \
+		--ramdisk $(PRODUCT_OUT)/ramdisk.img \
+		--cmdline "$(BOARD_KERNEL_CMDLINE)" \
+		--base $(BOARD_KERNEL_BASE) \
+		--pagesize $(BOARD_KERNEL_PAGESIZE) \
+		$(BOARD_MKBOOTIMG_ARGS) \
+		-o $(INSTALLED_BOOTIMAGE_TARGET)
+
+INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_ramdisk)
+	@echo ----- Making recovery image ------
+	$(hide) $(PREBUILT_MKBOOTIMG) \
+		--kernel $(TARGET_PREBUILT_KERNEL) \
+		--ramdisk $(PRODUCT_OUT)/ramdisk-recovery.img \
+		--cmdline "$(BOARD_KERNEL_CMDLINE)" \
+		--base $(BOARD_KERNEL_BASE) \
+		--pagesize $(BOARD_KERNEL_PAGESIZE) \
+		$(BOARD_MKBOOTIMG_ARGS) \
+		-o $(INSTALLED_RECOVERYIMAGE_TARGET)
+	@echo ----- Made recovery image -------- $@
