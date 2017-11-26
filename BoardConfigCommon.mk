d7356086aa6ebdf4090078182b9f363b5339f7d5
scoty755 <scoty755@gmail.com>
2017-11-26 16:15:31 +0900

    Revert "b3-common: Use prebuilt device tree blobs"
    This reverts commit bb2c31765efc23fddafe33b70f6efb1fa52c2b29.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -48,7 +48,7 @@ BOARD_KERNEL_CMDLINE := console=none androidboot.hardware=qcom user_debug=31 ehc
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01f88000 --dt $(COMMON_PATH)/../$(TARGET_DEVICE)/dt.img --tags_offset 0x01d88000
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01f88000 --tags_offset 0x01d88000
 BOARD_RAMDISK_OFFSET := 0x02000000
 ENABLE_CPUSETS := true
 TARGET_KERNEL_ARCH := arm64
