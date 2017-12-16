af1531fe774b340f80e4576ea9e0d4476fa63c82
scoty755 <scoty755@gmail.com>
2017-12-16 21:23:27 +0900

    b3: Refile dalvik-heap make rules

--- b3.mk
+++ b3.mk
@@ -31,7 +31,7 @@ PRODUCT_AAPT_PREF_CONFIG := 560dpi
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Add WiFi Config files
